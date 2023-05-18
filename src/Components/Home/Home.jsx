b4cbf4fcfbc54b863ea67f3fe93958a77e4e51eb
nabeel365 <choudhurinabeel@gmail.com>
2023-05-18 11:14:31 +0530

    navbar and footer created

---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Footer from '../Footer/Footer';
+import Navigation from '../Navigation/Navigation';
+
+const Home = () => {
+    return (
+        <div>
+            <Navigation></Navigation>
+            home
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default Home;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import './index.css'
+
+import Home from './Components/Home/Home.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home></Home> ,
+  },
+]);
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+   <RouterProvider router={router} />
   </React.StrictMode>,
 )
